37269b02c8e2155155814b77d6d3c6607d316d2c
Diego Lopez <sadiegolopez@gmail.com>
2022-06-24 20:20:44 -0300

    Backend proyecto de gestión de pacientes - Taller 4

---
+++ gestion-pacientes/src/main/java/com/gestionpacientes/repositories/pacienteRepository.java
@@ -0,0 +1,16 @@
+package com.gestionpacientes.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gestionpacientes.model.Paciente;
+
+@Repository
+public interface pacienteRepository extends JpaRepository<Paciente , Integer> {
+	Optional<Paciente> findByNombre(String nombre);
+	Optional<Paciente> findByDNI(int DNI);
+	Boolean existsByNombre(String nombre);
+	Boolean existsByDNI(int DNI);
+}
